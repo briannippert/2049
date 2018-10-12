5cf4baa4a707c0df2ae9b576fd6947a341d5f90d
Andrew <andrew.stencavage@engility.com>
2018-10-12 12:46:41 -0400

    change keypress

--- game.js
+++ game.js
@@ -1,14 +1,14 @@
-document.addEventListener("keypress", function(event) {
-    if (event.keyCode == 97) {
+document.addEventListener("keydown", function(event) {
+    if (event.keyCode == 37) {
         console.log('Left')
     }
-    if (event.keyCode == 119) {
+    if (event.keyCode == 38) {
         console.log('Up')
     }
-    if (event.keyCode == 100) {
+    if (event.keyCode == 39) {
         console.log('Right')
     }
-    if (event.keyCode == 115) {
+    if (event.keyCode == 40) {
         console.log('Down')
     }
 })
