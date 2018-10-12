d37d856facb39eee25aa5afa80e035ad3d62f63d
Stencavage <Andrew.Stencavage@ENGILITY.COM>
2018-10-12 14:48:22 -0400

    added random cell function

--- game.js
+++ game.js
@@ -21,24 +21,43 @@ let grid = [
     [null,2,2,null]
 ]
 
+function addCell(){
+    while(true){
+        var row = Math.floor(Math.random() * 4);
+        var col = Math.floor(Math.random() * 4);
+        if(!grid[row][col]){
+            var val = 2 * Math.ceil(Math.random() * 2);
+            grid[row][col] = val;
+            console.table(grid);
+            return;
+        }
+    }
+}
+
 function moveLeft(){
     grid.forEach(function(row){
         for (col in row){
-            if(!row[col] || col == 0){
+            if(!row[col + 1] || col == 3){
                 continue;
             }
+            if(row[col])
             console.log(row[col])
+            
         }
     })
     console.log('Left')
 }
+
 function moveUp(){
     console.log('Up')
 }
+
 function moveRight(){
     console.log('Right')
 }
+
 function moveDown(){
     console.log('Down')
 }
+
 console.table(grid)
